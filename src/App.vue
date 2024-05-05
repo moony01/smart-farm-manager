ba35124abb13ab916feb5e03b981d39d07b7004e
mhhan <mun01180@gmail.com>
2024-05-05 22:43:21 +0900

    퍼블 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="chat-body">
     <div class="chat-body-char">
+
+    </div>
+
+    <div id="chatContainer" class="chat-body-chatting">
+      <!-- 채팅 내용 -->
       <div class="q-pa-md row justify-center">
-        <div style="width: 100%; max-width: 400px">
+        <div style="width: 100%">
           <q-chat-message
             name="me"
             avatar="https://cdn.quasar.dev/img/avatar4.jpg"
@@ -16,9 +21,45 @@
             :text="[`doing fine, how r you?`]"
             stamp="4 minutes ago"
           />
+          <q-chat-message
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+            :text="['hey, how are you?']"
+            sent
+            stamp="7 minutes ago"
+          />
+          <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+            :text="[`doing fine, how r you?`]"
+            stamp="4 minutes ago"
+          />
+          <q-chat-message
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+            :text="['hey, how are you?']"
+            sent
+            stamp="7 minutes ago"
+          />
         </div>
       </div>
     </div>
+
+    <div class="chat-body-input">
+      <q-input 
+        filled 
+        class="chat-body-input-usr-message"
+        v-model="ph" 
+        placeholder="메시지를 입력하세요."
+      />
+      <q-btn
+        padding="md"
+        color="primary"
+        round
+        icon="eco"
+      />
+    </div>
+
   </div>
 </template>
 
