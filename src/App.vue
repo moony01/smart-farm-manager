5b132057832ae483a307400eba951b690974cead
mhhan <mun01180@gmail.com>
2024-05-06 10:29:13 +0900

    header추가

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
 <template>
+  <q-layout view="lHh lpr lFf" container style="height: 98px" class="shadow-2 rounded-borders">
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+      </q-avatar>
+
+      <q-toolbar-title>Quasar Framework</q-toolbar-title>
+
+      <q-btn flat round dense icon="whatshot" />
+    </q-toolbar>
+
+    <q-tabs v-model="tab">
+      <q-tab name="images" label="Images" />
+      <q-tab name="videos" label="Videos" />
+      <q-tab name="articles" label="Articles" />
+    </q-tabs>
+  </q-header>
+</q-layout>
   <div class="chat-body">
     <div class="chat-body-char">
 
@@ -13,33 +33,28 @@
             avatar="https://cdn.quasar.dev/img/avatar4.jpg"
             :text="['hey, how are you?']"
             sent
-            stamp="7 minutes ago"
           />
           <q-chat-message
             name="Jane"
             avatar="https://cdn.quasar.dev/img/avatar3.jpg"
             :text="[`doing fine, how r you?`]"
-            stamp="4 minutes ago"
           />
           <q-chat-message
             name="me"
             avatar="https://cdn.quasar.dev/img/avatar4.jpg"
             :text="['hey, how are you?']"
             sent
-            stamp="7 minutes ago"
           />
           <q-chat-message
             name="Jane"
             avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-            :text="[`doing fine, how r you?`]"
-            stamp="4 minutes ago"
+            :text="[`doing fine, how r you?doing fine, how r you?doing fine, how r you?doing fine, how r you?doing fine, how r you?`]"
           />
           <q-chat-message
             name="me"
             avatar="https://cdn.quasar.dev/img/avatar4.jpg"
             :text="['hey, how are you?']"
             sent
-            stamp="7 minutes ago"
           />
         </div>
       </div>
